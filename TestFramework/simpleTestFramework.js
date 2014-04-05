87035a4f025a8c54a4399bfa0483382dffe6a9f5
Richard Casto <rcasto92@gmail.com>
2014-04-04 20:35:28 -0400

    Started working on simple testing framework, after this is done start working on pong

---
+++ TestFramework/simpleTestFramework.js
@@ -0,0 +1,24 @@
+/*
+    This method asserts that the input expression is true
+    expr - expression that evaluates to true or false
+    desc - description of the assertion being tested
+*/
+function assertTrue(expr, desc) {
+    
+}
+
+/*
+    This method asserts that the input expression is false
+    expr - expression that evaluates to true or false
+    desc - description of the assertion being tested
+*/
+function assertFalse(expr, desc) {
+
+}
+
+/*
+    A TestSuite is essentially a collection of tests
+*/
+function TestSuite() {
+    this.tests = [];
+}
